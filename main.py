1ea49f48ee7ef462523da40db68329e0b0343914
MazurkiewiczMatt <mateusz.a.mazurkiewicz@gmail.com>
2020-10-17 12:43:16 +0200

    Insights added

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from datetime import date
-from utility import read_settings, get_keywords
+import datetime
+from utility import read_settings, get_keywords, daterange
 from web_scraper import get_comments
 import pandas as pd
 from dictionary_sentiment_analysis import dictionary_sentiment_check
@@ -12,9 +12,11 @@ print(keywords)
 
 df = pd.DataFrame()
 
-for subreddit in settings[-1]:
-    print("Checking " + subreddit)
-    df = df.append(get_comments(subreddit, settings[1], settings[2], keywords))
+for start_date in daterange(settings[1], settings[2]):
+    end_date = start_date + datetime.timedelta(days=1)
+    for subreddit in settings[-1]:
+        print("Checking " + subreddit)
+        df = df.append(get_comments(subreddit, start_date, end_date, keywords))
 
 vectorized_sentiment_check = numpy.vectorize(dictionary_sentiment_check)
 df['Dictionary_bullishness'] = vectorized_sentiment_check(df['Body'])
@@ -29,4 +31,9 @@ print(df)
 dictionary_metric = df.drop(columns=['ID', 'Author', 'Body'])
 
 print(dictionary_metric)
+
+insights = []
+
+for subreddit in dictionary_metric['Subreddit'].tolist():
+    insights.append(dictionary_metric[dictionary_metric['Subreddit'] == subreddit])
 
